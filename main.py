764e3547f7d66257fdb14294d905b920ad60b665
alexeisanko <alexeisanko@gmail.com>
2024-02-29 22:48:32 +0300

    polling tg bot

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+import os
+import asyncio
+import logging
+import sys
+
+from aiogram import Bot, Dispatcher
 import motor.motor_asyncio
+
+import handlers
 
 client = motor.motor_asyncio.AsyncIOMotorClient()
 db = client["main_db"]
 collection = db["rlt_test_collection"]
+
+
+async def main():
+    bot = Bot(os.environ['TG_TOKEN'])
+    dp = Dispatcher()
+    dp.include_router(handlers.router)
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
