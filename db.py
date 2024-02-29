b7fc2d5be30a40737607d6098c854ef4c60be8a2
alexeisanko <alexeisanko@gmail.com>
2024-02-29 22:05:19 +0300

    method get_aggregate_data from mongoDB

--- db.py
+++ db.py
@@ -1,8 +1,24 @@
+from datetime import datetime
+
 from main import collection
 
 
 class MongoDB:
     _collection = collection
+
+    @classmethod
+    async def get_data(cls, start_dt: datetime, end_dt: datetime):
+        cursor = cls._collection.find({"dt": {"$gte": start_dt, "$lt": end_dt}})
+        return [obj async for obj in cursor]
+
+    @classmethod
+    async def get_filter_aggregate_data(cls, start_dt: datetime, end_dt: datetime):
+        pipline = [
+            {"$match": {"dt": {"$gte": start_dt, "$lt": end_dt}}},
+            {"$group": {"_id": start_dt, "total": {"$sum": "$value"}}}
+        ]
+        cursor = cls._collection.aggregate(pipline)
+        return [obj async for obj in cursor]
 
     @classmethod
     async def insert_data(cls, data):
